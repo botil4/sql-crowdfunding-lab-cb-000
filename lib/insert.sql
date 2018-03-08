c31cdad155b1761c7347e567d868c64961230054
Travis Clarkson <tk1667@gmail.com>
2018-03-08 03:46:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,54 +10,56 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ("help my hamster", "animal", 100, "2017-02-02", "2018-03-02"),
 ("put on concert", "music", 10000, "2017-011-02", "2018-02-02");
 
-INSERT INTO users (name, age) VALUES ("Bob", 20);
-INSERT INTO users (name, age) VALUES ("Sue", 30);
-INSERT INTO users (name, age) VALUES ("Jill", 25);
-INSERT INTO users (name, age) VALUES ("Travis", 29);
-INSERT INTO users (name, age) VALUES ("Steve", 40);
-INSERT INTO users (name, age) VALUES ("Jamie", 30);
-INSERT INTO users (name, age) VALUES ("Dave", 18);
-INSERT INTO users (name, age) VALUES ("Adam", 35);
-INSERT INTO users (name, age) VALUES ("Brian", 37);
-INSERT INTO users (name, age) VALUES ("Jim", 22);
-INSERT INTO users (name, age) VALUES ("Barb", 25);
-INSERT INTO users (name, age) VALUES ("Brenda", 30);
-INSERT INTO users (name, age) VALUES ("Amanda", 32);
-INSERT INTO users (name, age) VALUES ("Brittany", 26);
-INSERT INTO users (name, age) VALUES ("Sandy", 50);
-INSERT INTO users (name, age) VALUES ("Bill", 45);
-INSERT INTO users (name, age) VALUES ("Mike", 15);
-INSERT INTO users (name, age) VALUES ("John", 60);
-INSERT INTO users (name, age) VALUES ("Larry", 55);
-INSERT INTO users (name, age) VALUES ("Dan", 21);
+INSERT INTO users (name, age) VALUES 
+("Bob", 20),
+("Sue", 30).
+("Jill", 25).
+("Travis", 29).
+("Steve", 40).
+("Jamie", 30).
+("Dave", 18).
+("Adam", 35).
+("Brian", 37).
+("Jim", 22).
+("Barb", 25).
+("Brenda", 30).
+("Amanda", 32).
+("Brittany", 26).
+("Sandy", 50).
+("Bill", 45).
+("Mike", 15).
+("John", 60).
+("Larry", 55).
+("Dan", 21);
 
-INSERT INTO pledges (user_id, project_id, amount) VALUES (1, 1, 100);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (2, 6, 200);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (3, 3, 300);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (4, 7, 500);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (5, 10, 100);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (6, 2, 50);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (7, 4, 70);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (8, 5, 80);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (9, 8, 200);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (10, 9, 10);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (11, 1, 100);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (12, 2, 20);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (13, 8, 30);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (14, 9, 100);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (15, 10, 1000);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (16, 3, 100);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (17, 6, 400);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (18, 7, 60);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (19, 5, 80);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (20, 8, 1000);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (1, 8, 1000);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (2, 8, 5000);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (5, 8, 800);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (9, 7, 250);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (11, 7, 300);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (15, 1, 700);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (17, 1, 50);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (4, 8, 900);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (6, 4, 150);
-INSERT INTO pledges (user_id, project_id, amount) VALUES (19, 2, 20);
+INSERT INTO pledges (user_id, project_id, amount) VALUES 
+(1, 1, 100),
+(2, 6, 200),
+(3, 3, 300),
+(4, 7, 500),
+(5, 10, 100),
+(6, 2, 50),
+(7, 4, 70),
+(8, 5, 80),
+(9, 8, 200),
+(10, 9, 10),
+(11, 1, 100),
+(12, 2, 20),
+(13, 8, 30),
+(14, 9, 100),
+(15, 10, 1000),
+(16, 3, 100),
+(17, 6, 400),
+(18, 7, 60),
+(19, 5, 80),
+(20, 8, 1000),
+(1, 8, 1000),
+(2, 8, 5000),
+(5, 8, 800),
+(9, 7, 250),
+(11, 7, 300),
+(15, 1, 700),
+(17, 1, 50),
+(4, 8, 900),
+(6, 4, 150),
+(19, 2, 20);
